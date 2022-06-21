4b53990eabcf9b26d38dce7df18f0f5e042564e8
vitorsandrade <vitorsantos.dandrade@gmail.com>
2022-06-21 20:10:50 -0300

    first commit

---
+++ src/main/java/com/vitor/forestequipment/ForestequipmentApplication.java
@@ -0,0 +1,13 @@
+package com.vitor.forestequipment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForestequipmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForestequipmentApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/vitor/forestequipment/repositories/EquipmentRepository.java
@@ -0,0 +1,9 @@
+package com.vitor.forestequipment.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.vitor.forestequipment.entities.Equipment;
+
+public interface EquipmentRepository extends JpaRepository<Equipment, Long>{
+
+}
