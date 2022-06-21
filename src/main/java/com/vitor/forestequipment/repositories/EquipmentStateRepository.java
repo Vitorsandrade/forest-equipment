a0fdd49549beb20e6931b561b3a4dc62ff81f872
vitorsandrade <vitorsantos.dandrade@gmail.com>
2022-06-21 20:46:54 -0300

    Repositories

---
+++ src/main/java/com/vitor/forestequipment/repositories/EquipmentStateRepository.java
@@ -0,0 +1,9 @@
+package com.vitor.forestequipment.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.vitor.forestequipment.entities.EquipmentState;
+
+public interface EquipmentStateRepository extends JpaRepository<EquipmentState, Long> {
+
+}
